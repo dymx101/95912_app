24fca4d447a1d840b9ca007067d8142e4605557e
Daniel Dong <ydong@gagein.com>
2015-06-02 18:20:43 +0800

    tuning

--- Taasky 2/Taasky/AppDelegate.swift
+++ Taasky 2/Taasky/AppDelegate.swift
@@ -26,10 +26,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     let students = PMStudent.getAllStudents()
     if (students.count == 0) {
-        let student = PMStudent.createStudent("董一鸣", studentID: 24)
-        student.qq = "2757273"
-        student.roomID = 109
-        student.save()
+        
+        var newStudents: [PMStudent] = []
+        
+        newStudents.append(PMStudent.createStudent("朱建玲", studentID: 1))
+        newStudents.append(PMStudent.createStudent("杨怡", studentID: 2))
+        newStudents.append(PMStudent.createStudent("周莉", studentID: 3))
+        newStudents.append(PMStudent.createStudent("田丹丹", studentID: 4))
+        newStudents.append(PMStudent.createStudent("薛新峰", studentID: 5))
+        newStudents.append(PMStudent.createStudent("李红", studentID: 6))
+        newStudents.append(PMStudent.createStudent("冷雪浩", studentID: 7))
+        newStudents.append(PMStudent.createStudent("王晓川", studentID: 8))
+        newStudents.append(PMStudent.createStudent("章卫忠", studentID: 9))
+        
+        newStudents.append(PMStudent.createStudent("陈晓敏", studentID: 10))
+        newStudents.append(PMStudent.createStudent("查明高", studentID: 11))
+        newStudents.append(PMStudent.createStudent("陈世武", studentID: 12))
+        newStudents.append(PMStudent.createStudent("封亚勇", studentID: 13))
+        newStudents.append(PMStudent.createStudent("崔永", studentID: 14))
+        newStudents.append(PMStudent.createStudent("余青松", studentID: 15))
+        newStudents.append(PMStudent.createStudent("王长波", studentID: 16))
+        newStudents.append(PMStudent.createStudent("阮鸥", studentID: 17))
+        newStudents.append(PMStudent.createStudent("贺能", studentID: 18))
+        newStudents.append(PMStudent.createStudent("马四祥", studentID: 19))
+        
+        newStudents.append(PMStudent.createStudent("邱拥军", studentID: 20))
+        newStudents.append(PMStudent.createStudent("唐密", studentID: 21))
+        newStudents.append(PMStudent.createStudent("向晋龙", studentID: 22))
+        newStudents.append(PMStudent.createStudent("周若来", studentID: 23))
+        newStudents.append(PMStudent.createStudent("董一鸣", studentID: 24))
+        newStudents.append(PMStudent.createStudent("谢臻", studentID: 25))
+        newStudents.append(PMStudent.createStudent("郑小斌", studentID: 26))
+        newStudents.append(PMStudent.createStudent("李涛鹏", studentID: 27))
+        newStudents.append(PMStudent.createStudent("向睿", studentID: 28))
+        newStudents.append(PMStudent.createStudent("孙云", studentID: 29))
+        
+        newStudents.append(PMStudent.createStudent("高向鹏", studentID: 30))
+        newStudents.append(PMStudent.createStudent("张劲涛", studentID: 31))
+        newStudents.append(PMStudent.createStudent("董魁", studentID: 32))
+        newStudents.append(PMStudent.createStudent("王丽", studentID: 33))
+        newStudents.append(PMStudent.createStudent("李海京", studentID: 34))
+
+        for student in newStudents {
+            student.save()
+        }
+        
     } else {
         print(students)
     }
